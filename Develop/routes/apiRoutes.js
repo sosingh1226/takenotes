f59eaf2bd274f79a014bd2970caf8eea606572df
soumyas <so.singh1226@gmail.com>
2020-12-15 17:31:40 -0500

    updated apiRoutes

--- Develop/routes/apiRoutes.js
+++ Develop/routes/apiRoutes.js
@@ -1,36 +1,63 @@
 const router = require("express").Router();
-const path = require("path");
 const fs = require("fs");
+const uuid = require("uuid");
 
-router.get("/api/notes", (req, res) => {
-    fs.readFile("../db/db.json", "utf8", (err, data) => {
+// it would look like- /api/notes
+
+router.get("/notes", (req, res) => {
+    fs.readFile("./db/db.json", "utf8", (err, data) => {
     if (err) throw err;
 
     const allNotes = JSON.parse(data);
     console.log (allNotes);
+
+    res.json(allNotes);
     });
 });
 
-router.post("/api/notes", (req, res) => {
+router.post("/notes", (req, res) => {
     console.log(req.body);
-    fs.readFile("../db/db.json", "utf8", (err, data) =>{
+
+    fs.readFile("./db/db.json", "utf8", (err, data) =>{
         if (err) throw err;
         const allNotes = JSON.parse(data);
-        
-        allNotes.push({
+        const newNote = {
             title: req.body.title,
-            text: req.body.text
-        });
+            text: req.body.text,
+            id: uuid.v1()
+        };
+
+        allNotes.push(
+            newNote
+        );
         console.log(allNotes);
     
-        fs.writeFile("../db/db.json", JSON.stringify(allNotes), (err) =>{
-            if (err) return res.JSON ({ err: "problem adding"});
-            return res.json({msg:"successfully added"});
+        fs.writeFile("./db/db.json", JSON.stringify(allNotes), (err) =>{
+            if (err) res.json ({ err: "problem adding"});
+            res.json({msg:"successfully added"});
         });
     });
 });
 
+router.delete("/notes/:id", (req, res) => {
+    console.log(req.body);
 
+    fs.readFile("./db/db.json", "utf8", (err, data) =>{
+        if (err) throw err;
+        const allNotes = JSON.parse(data);
+        const delNote = req.params.id;
 
+        const result = allNotes.filter(note => note.id != delNote);
+            
+        console.log(allNotes);
+        console.log(result);
+    
+        fs.writeFile("./db/db.json", JSON.stringify(result), (err) =>{
+            if (err) res.json ({ err: "problem deleting"});
+            res.json(result);
+            res.json({msg:"successfully deleted"});
+        });
+    });
+});
 
 module.exports = router;
