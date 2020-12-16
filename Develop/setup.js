98a60f4275932976061ee30f65bb929f49ebd2ba
soumyas <so.singh1226@gmail.com>
2020-12-15 19:31:33 -0500

    commit with new readme without the deployed link

--- Develop/setup.js
+++
@@ -1,8 +0,0 @@
-const fs = require("fs");
-
-const noteStore = []
-
-fs.writeFile("./../db/db.json", JSON.stringify(noteStore), (err) => {
-    if (err) throw err;
-    console.log("Finished");
-});
